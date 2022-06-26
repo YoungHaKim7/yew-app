78689e116a5b3e76bc2bf5807f9724a52026a474
YoungHa Kim <ytok1108@aol.com>
2022-06-26 21:40:03 +0900

    html ___ img link

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,19 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World"}</h1>
+    <>
+        <h1>{ "RustConf Explorer"}</h1>
+        <div>
+            <h3>{"Videos to watch"}</h3>
+            <p>{"Jogn Doe: Building and breadking things"}</p>
+            <p>{"Jane Smith: The development process"}</p>
+            <p>{"Tom Jerry: Mouseless development"}</p>
+        </div>
+        <div>
+            <h3>{"John Doe : Building and breadking things"}</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    </>
     }
 }
 
